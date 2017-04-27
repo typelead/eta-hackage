dfccdd5c983857c95744148ec4f1a8698765861e
Rahul Muttineni <rahulmutt@gmail.com>
2017-04-27 10:00:01 +0530

    Update eta-hackage script

--- scripts/eta-install.sh
+++ scripts/eta-install.sh
@@ -13,11 +13,6 @@ else
     git submodule sync
     _subs=$(git submodule update --recursive)
 
-    if [ "$_pull" == "Already up-to-date." ] && [ -z "$_subs" ]; then
-  	    echo "Already up-to-date."
-  	    exit 0
-    fi
-
     ./cleaninstall.sh
     etlas update
 fi
