9f864a255bcf886b248a080b195e7b5130b9053c
Rahul Muttineni <rahulmutt@gmail.com>
2017-05-16 08:23:32 +0530

    Updated build script

--- scripts/eta-install.sh
+++ scripts/eta-install.sh
@@ -1,18 +1,33 @@
-# From: https://github.com/haskell-works/eta-kafka-client/blob/master/scripts/eta-install.sh
 #!/usr/bin/env bash
 if [ ! -d "${HOME}/eta" ]; then
-    echo "Installing ETA"
+    echo "Cloning eta"
     cd ${HOME}
     git clone --recursive https://github.com/typelead/eta
-    cd eta
-    ./install.sh
-else
-    echo "Updating ETA"
-    cd ${HOME}/eta
-    _pull=$(git pull)
-    git submodule sync
-    _subs=$(git submodule update --recursive)
-
-    ./cleaninstall.sh
-    etlas update
 fi
+
+cd ${HOME}/eta
+git fetch --all --tags -p
+git pull --all
+
+if [ -n "${ETA_BRANCH}" ]; then
+    echo "Building eta branch ${ETA_BRANCH}"
+    git checkout "${ETA_BRANCH}"
+
+elif [ -n "${ETA_TAG}" ]; then
+    echo "Building eta tag ${ETA_TAG}"
+    git checkout "${ETA_TAG}"
+
+elif [ -n "${ETA_PR}" ]; then
+    echo "Building eta pull request ${ETA_PR}"
+    ETA_PR_NO=$(echo "${ETA_PR}" | sed -e 's/.*\///')
+    ETA_BRANCH="pr-${ETA_PR_NO}"
+    git fetch origin "pull/${ETA_PR_NO}/head:${ETA_BRANCH}"
+    git checkout "${ETA_BRANCH}"
+else
+    echo "Building without trigger"
+    git checkout master
+fi
+
+git submodule sync
+git submodule update --init --recursive
+./install.sh
