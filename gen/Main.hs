396f3e5f1e391e47bd411eb6a70cd7aacfa55c0c
Rahul Muttineni <rahulmutt@gmail.com>
2018-12-07 17:12:34 +0530

    Add WORKING file

--- gen/Main.hs
+++ gen/Main.hs
@@ -3,18 +3,30 @@ import System.FilePath ((</>), dropExtension)
 import Data.List
 
 main = do
-  patchFiles <- getDirectoryContents "patches"
+  pkgVers1 <- fromPatchesFolder
+  pkgVers2 <- fromWorkingFile
   let preferences = map toPreference
                   . groupBy (\(a1,_) (a2,_) -> a1 == a2)
                   . map splitPkgVersion
                   . sort
                   . nub
-                  . map dropExtension
-                  . filter (\p -> p `notElem` ["",".",".."])
-                  $ patchFiles
+                  $ pkgVers1 ++ pkgVers2
       splitPkgVersion pkgVer = (reverse (drop 1 rest), reverse reverseVer)
         where (reverseVer, rest) = break (== '-') $ reverse pkgVer
       toPreference pairs@((pkg,_):_) =
         "pref-ver: " ++ pkg ++ " " ++ intercalate " || " versions
         where versions = map ((\v -> "== " ++ v) . snd) pairs
   writeFile "PREFERENCES" (unlines preferences)
+
+
+fromPatchesFolder = do
+  patchFiles <- getDirectoryContents "patches"
+  return . map dropExtension
+         . filter (\p -> p `notElem` ["",".",".."])
+         $ patchFiles
+
+fromWorkingFile = do
+  contents <- readFile "WORKING"
+  let makePkgVers l = map ((pkg <> "-") <>) vers
+        where (pkg:vers) = words l
+  return $ concatMap makePkgVers $ lines $ contents
